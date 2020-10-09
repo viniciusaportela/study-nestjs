c78a0656c5a5ab1781b236ce9bf36979391d2784
viniciusaportela <vinicius.portela.stm@gmail.com>
2020-10-09 14:50:36 -0300

    Fix configService selector in config module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from './config.service';
 
 import { DevelopmentConfigService } from './development-config.service';
 import { ProductionConfigService } from './production-config.service';
@@ -6,11 +7,12 @@ import { ProductionConfigService } from './production-config.service';
 @Module({
   providers: [
     {
-      provide: ConfigModule,
-      useClass: process.env.development
+      provide: ConfigService,
+      useClass: process.env.NODE_ENV !== 'production'
         ? DevelopmentConfigService
         : ProductionConfigService,
     },
   ],
+  exports: [ConfigService]
 })
 export class ConfigModule {}
