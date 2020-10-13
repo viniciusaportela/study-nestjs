d0ecbe69c1ade1ea130ac627df1357d53031bd19
viniciusaportela <vinicius.portela.stm@gmail.com>
2020-10-13 12:05:13 -0300

    Add e2e test to route /v1/users/ (POST)

--- gateway/test/app.e2e-spec.ts
+++ gateway/test/app.e2e-spec.ts
@@ -1,10 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Roles } from '../src/@types/roles';
+import { RpcExceptionFilter } from '../src/filters/rpc-exception.filter';
+import { HttpExceptionFilter } from '../src/filters/http-exception.filter';
+import { HttpAdapterHost } from '@nestjs/core';
+import { json } from 'body-parser';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let baseUrl: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +18,78 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+  
+    const { httpAdapter } = app.get(HttpAdapterHost);
+
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new RpcExceptionFilter(httpAdapter), new HttpExceptionFilter());
+
+    app.enableCors();
+    app.use(json());
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await app.close();
+  })
+
+  describe('/v1/users (POST)', () => {
+    baseUrl = '/v1/users';
+
+    it('should throw a error if not give name', async () => {
+      const res = await request(app.getHttpServer())
+        .post(baseUrl).send({})
+
+      expect(res.status).toBe(422);
+      expect(res.body).toHaveProperty('error', 'Bad Request');
+      expect(res.body).toHaveProperty('message');
+      expect(res.body.message).toContain('name should not be empty');
+    })
+
+    it('should throw a error if not give password', async () => {
+      const res = await request(app.getHttpServer())
+        .post(baseUrl).send({})
+
+      expect(res.status).toBe(422);
+      expect(res.body).toHaveProperty('error', 'Bad Request');
+      expect(res.body).toHaveProperty('message');
+      expect(res.body.message).toContain('password should not be empty')
+    })
+
+    it('should throw a error if not give level', async () => {
+      const res = await request(app.getHttpServer())
+        .post(baseUrl).send({})
+
+      expect(res.status).toBe(422);
+      expect(res.body).toHaveProperty('error', 'Bad Request');
+      expect(res.body).toHaveProperty('message');
+      expect(res.body.message).toContain('level should not be empty')
+    })
+
+    it('should throw a error if level is not in Roles', async () => {
+      const res = await request(app.getHttpServer())
+        .post(baseUrl).send({})
+
+      expect(res.status).toBe(422);
+      expect(res.body).toHaveProperty('error', 'Bad Request');
+      expect(res.body).toHaveProperty('message');
+      
+      const expectedString = 'level must be one of the following values:';
+      const matches = res.body.message.filter(msg => msg.includes(expectedString)).length;
+      expect(matches).toBeGreaterThan(0);
+    })
+
+    it('should successfully create user', async () => {
+      const res = await request(app.getHttpServer())
+      .post(baseUrl)
+          .send({
+            name: 'test',
+            password: '81dc9bdb52d04dc20036dbd8313ed055',
+            level: Roles.USER
+          });
+
+      expect(res.status).toBe(201);
+    })
   });
 });
