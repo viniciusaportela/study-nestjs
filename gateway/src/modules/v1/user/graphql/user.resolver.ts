104b8b322746b38ceaccc0e7bd603bd207f40bbb
viniciusaportela <vinicius.portela.stm@gmail.com>
2020-10-13 01:21:53 -0300

    Improve authenticateUser mutation error handling

--- gateway/src/modules/v1/user/graphql/user.resolver.ts
+++ gateway/src/modules/v1/user/graphql/user.resolver.ts
@@ -1,5 +1,5 @@
 import { UseFilters } from "@nestjs/common";
-import { ApolloError } from 'apollo-server-express';
+import { ApolloError, AuthenticationError } from 'apollo-server-express';
 import { Args, Resolver, Query, Mutation } from "@nestjs/graphql";
 import { Roles } from "../../../../@types/roles";
 import { GraphQlExceptionFilter } from "../../../../filters/grapql-exception.filter";
@@ -7,11 +7,11 @@ import { UserService } from "../user.service";
 import { AuthenticationToken, User } from "./user.model";
 
 @Resolver()
+@UseFilters(new GraphQlExceptionFilter())
 export class UserResolver {
   constructor(private userService: UserService){}
   
   @Query(returns => User, { name: 'user' })
-  @UseFilters(new GraphQlExceptionFilter())
   async getUser(@Args('_id') _id: string) {
     return this.userService.get(_id);
   }
@@ -31,7 +31,13 @@ export class UserResolver {
     try {
       return { token: await this.userService.authenticate({name, password}).toPromise() };
     } catch (e) {
-      throw new ApolloError('Internal Server Error', 'INTERNAL SERVER ERROR')
+      if (e.error.status === 401) {
+        throw new AuthenticationError('wrong credentials')
+      } else if (e.error.status === 404) {
+        throw new ApolloError('Not Found', 'NOT FOUND')
+      } else {
+        throw new ApolloError('Internal Server Error', 'INTERNAL SERVER ERROR')
+      }
     }
   }
 }
